abd85b3ea08a903108843f0b81b1f4d6a3c31edb
tschut <tschut@gmail.com>
2014-01-14 19:45:04 +0100

    Add gson and first simple RestClient

---
+++ app/src/com/lolchair/lolchair/LolchairRestClient.java
@@ -0,0 +1,11 @@
+package com.lolchair.lolchair;
+
+import org.androidannotations.annotations.rest.Get;
+import org.androidannotations.annotations.rest.Rest;
+import org.springframework.http.converter.json.GsonHttpMessageConverter;
+
+@Rest(rootUrl = "http://www.lolchair.com/api", converters = GsonHttpMessageConverter.class)
+public interface LolchairRestClient {
+    @Get("/get_recent_posts")
+    Post[] getRecentPosts();
+}
---
+++ app/src/com/lolchair/lolchair/Post.java
@@ -0,0 +1,5 @@
+package com.lolchair.lolchair;
+
+public class Post {
+
+}
